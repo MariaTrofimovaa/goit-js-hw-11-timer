672a5d723b302af2c7b706a2d11d687ce7676f22
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-05-16 19:29:00 +0300

    finished timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,43 +1,42 @@
-/*
-Создай плагин настраиваемого таймера, 
-который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться 
-в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
 
-preview
+    this.refs = {
+      days: document.querySelector('[data-value = days]'),
+      hours: document.querySelector('[data-value = hours]'),
+      mins: document.querySelector('[data-value = mins]'),
+      secs: document.querySelector('[data-value = secs]'),
+    };
+  }
 
-Плагин ожидает следующую HTML-разметку и показывает четыре цифры: 
-дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
-*/
+  updateTime() {
+    setInterval(() => {
+      this.startTime = new Date();
+      this.endTime = this.targetDate - this.startTime;
 
-// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
+      this.days = Math.floor(this.endTime / (1000 * 60 * 60 * 24));
+      this.hours = pad(
+        Math.floor((this.endTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      this.mins = pad(
+        Math.floor((this.endTime % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+      this.secs = pad(Math.floor((this.endTime % (1000 * 60)) / 1000));
 
-new CountdownTimer({
+      this.refs.days.textContent = `${this.days}`;
+      this.refs.hours.textContent = `${this.hours}`;
+      this.refs.mins.textContent = `${this.mins}`;
+      this.refs.secs.textContent = `${this.secs}`;
+    }, 1000);
+
+    const pad = value => String(value).padStart(2, '0');
+  }
+}
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Jul 11, 2021'),
 });
-// Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
-
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer.updateTime();
